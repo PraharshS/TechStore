79f078cf6fd52e1099b3cd557e4d37a93659c874
Praharsh Singh <praharsh535@gmail.com>
2020-08-06 16:39:04 +0530

    single image and sidecart image fixed

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -22,7 +22,7 @@ export default function SingleProductPage() {
               <div className="container">
                 <div className="row">
                   <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
-                    <img src={`../${img}`} alt="img" className="img-fluid" />
+                    <img src={img} alt="img" className="img-fluid" />
                   </div>
                   <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
                     <h5 className="text-title mb-4 ">Model : {title}</h5>
